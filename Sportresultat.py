18c54c932d4440b41ac28ad9f3cc663bd451866f
AndersHTest <anders.henang@student.nbi-handelsakademin.se>
2026-05-18 22:35:54 +0200

    Sportresultat version 1....

---
+++ Sportresultat.py
@@ -0,0 +1,6 @@
+print("Matchen är över, nu ska vi räkna ut resultatet!")
+
+tottenham_goals = input("Hur många mål gjorde Tottenham? ")
+liverpool_goals = input("Hur många mål gjorde Liverpool? ")
+
+print("Tottenham vann!")
