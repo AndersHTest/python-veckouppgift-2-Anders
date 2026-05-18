5f84ec494d9e1607ba13b4878e4b2294ab07b6db
AndersHTest <anders.henang@student.nbi-handelsakademin.se>
2026-05-18 22:55:41 +0200

    Sportresultat version 3.

--- Sportresultat.py
+++ Sportresultat.py
@@ -2,10 +2,16 @@ print("Matchen är över, nu ska vi räkna ut resultatet!")
 
 tottenham_goals = int(input("Hur många mål gjorde Tottenham? "))
 liverpool_goals = int(input("Hur många mål gjorde Liverpool? "))
+tottenham_differens = 0
+liverpool_differens = 0
 
 if tottenham_goals > liverpool_goals:
-    print("Tottenham vann!")
+    print(f"Tottenham vann med {tottenham_goals-liverpool_goals} mål!")
 elif tottenham_goals < liverpool_goals:
-    print("Liverpool vann!")
+    print(f"Liverpool vann med {liverpool_goals-tottenham_goals} mål!")
 else:
-    print("Det blev lika")
+    print("Det blev oavgjort.")
+
+"""
+Testa med 2-1, 1-2 och 5-5.
+"""
