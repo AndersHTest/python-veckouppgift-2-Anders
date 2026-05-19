50e6eac6d0fe851afb5179e48d597c656303101d
AndersHTest <anders.henang@student.nbi-handelsakademin.se>
2026-05-19 11:19:33 +0200

    Miniräknare version 1.

---
+++ Miniräknare.py
@@ -0,0 +1,5 @@
+tal1 = int(input("Skriv in det första talet här: "))
+tal2 = int(input("Skriv in det andra talet här: "))
+tal3 = int(input("Skriv in det tredje talet här: "))
+
+print(tal1+tal2+tal3)
