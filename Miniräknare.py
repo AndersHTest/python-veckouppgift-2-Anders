60e2153d55f93bed90064c5074be1806cb093d1e
AndersHTest <anders.henang@student.nbi-handelsakademin.se>
2026-05-19 11:26:46 +0200

    Miniräknare version 2.

--- Miniräknare.py
+++ Miniräknare.py
@@ -2,4 +2,11 @@ tal1 = int(input("Skriv in det första talet här: "))
 tal2 = int(input("Skriv in det andra talet här: "))
 tal3 = int(input("Skriv in det tredje talet här: "))
 
-print(tal1+tal2+tal3)
+if tal1 > tal2 and tal1 > tal3:
+    print(f"{tal1} är det största talet.")
+elif tal2 > tal1 and tal2 > tal3:
+    print(f"{tal2} är det största talet.")
+elif tal3 > tal1 and tal3 > tal2:
+    print(f"{tal3} är det största talet.")
+else:
+    print(f"Kunde inte hitta det största talet. Är talen lika stora?")
