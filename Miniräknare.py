0f3e455b2a8039827d3622ef2c80ac083383d162
AndersHTest <anders.henang@student.nbi-handelsakademin.se>
2026-05-19 13:44:07 +0200

    Miniräknare version 4. Puh!

--- Miniräknare.py
+++ Miniräknare.py
@@ -2,12 +2,26 @@ tal1 = int(input("Skriv in det första talet här: "))
 tal2 = int(input("Skriv in det andra talet här: "))
 tal3 = int(input("Skriv in det tredje talet här: "))
 
-if (tal1 >= tal2) and (tal1 >= tal3):
+if tal1 >= tal2 and tal1 >= tal3:
     print(f"Det största talet är {tal1}.")
-elif (tal2 >= tal1) and (tal2 >= tal3):
+    if tal2 > tal3 and tal2 != tal1 and tal2 != tal3:
+        print(f"Det mellersta talet är {tal2}")
+    elif tal3 > tal2 and tal3 != tal1 and tal3 != tal2:
+        print(f"Det mellersta talet är {tal3}")
+
+elif tal2 >= tal1 and tal2 >= tal3:
     print(f"Det största talet är {tal2}.")
+    if tal1 > tal3 and tal1 != tal2 and tal1 != tal3:
+        print(f"Det mellersta talet är {tal1}")
+    elif tal3 > tal1 and tal3 != tal2 and tal3 != tal1:
+        print(f"Det mellersta talet är {tal3}")
+
 else:
     print(f"Det största talet är {tal3}.")
+    if tal1 > tal2 and tal1 != tal3 and tal1 != tal2:
+        print(f"Det mellersta talet är {tal1}")
+    elif tal2 > tal1 and tal2 != tal3 and tal2 != tal1 :
+        print(f"Det mellersta talet är {tal2}")
 
 if tal1 == tal2 == tal3:
     print("Alla tal är lika stora.")
